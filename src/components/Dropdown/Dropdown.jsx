8df071563863734035f37f6c31a5b574c5bd61e9
Rahul Pharthyal <157299833+rahulsinghpharthyal@users.noreply.github.com>
2025-06-12 11:03:17 +0530

    Delete src directory

--- src/components/Dropdown/Dropdown.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Dropdown = ({ children }) => {
-    return <ul className="dropdown-menu">{children}</ul>;
-};
-
-export default Dropdown;
--- src/customAxios/authAxios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    withCredentials: true,
-});
